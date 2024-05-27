5435d1d578b66d9ee1b31abeffad15faada689eb
高桥琳介 <18574945291@163.com>
2024-05-27 13:48:35 +0800

    demo

--- zero-leetcode/hot/fast_sort/fast_sort_test.go
+++ zero-leetcode/hot/fast_sort/fast_sort_test.go
@@ -2,3 +2,32 @@ package fast_sort
 
 // @Description
 // @Author 代码小学生王木木
+func sortArray(nums []int) []int {
+	var quick func(nums []int, left, right int) []int
+	quick = func(nums []int, left, right int) []int {
+		// 递归终止条件
+		if left > right {
+			return nil
+		}
+		// 左右指针及主元
+		i, j, pivot := left, right, nums[left]
+		for i < j {
+			// 寻找小于主元的右边元素
+			for i < j && nums[j] >= pivot {
+				j--
+			}
+			// 寻找大于主元的左边元素
+			for i < j && nums[i] <= pivot {
+				i++
+			}
+			// 交换i/j下标元素
+			nums[i], nums[j] = nums[j], nums[i]
+		}
+		// 交换元素
+		nums[i], nums[left] = nums[left], nums[i]
+		quick(nums, left, i-1)
+		quick(nums, i+1, right)
+		return nums
+	}
+	return quick(nums, 0, len(nums)-1)
+}
--- zero-leetcode/hot/merge_sor/merge_sort_test.go
+++ zero-leetcode/hot/merge_sor/merge_sort_test.go
@@ -2,3 +2,39 @@ package merge_sor
 
 // @Description
 // @Author 代码小学生王木木
+
+// 递归实现归并算法
+func sortArray(nums []int) []int {
+	merge := func(left, right []int) []int {
+		res := make([]int, len(left)+len(right))
+		var l, r, i int
+		// 通过遍历完成比较填入res中
+		for l < len(left) && r < len(right) {
+			if left[l] <= right[r] {
+				res[i] = left[l]
+				l++
+			} else {
+				res[i] = right[r]
+				r++
+			}
+			i++
+		}
+		// 如果left或者right还有剩余元素，添加到结果集的尾部
+		copy(res[i:], left[l:])
+		copy(res[i+len(left)-l:], right[r:])
+		return res
+	}
+	var sort func(nums []int) []int
+	sort = func(nums []int) []int {
+		if len(nums) <= 1 {
+			return nums
+		}
+		// 拆分递归与合并
+		// 分割点
+		mid := len(nums) / 2
+		left := sort(nums[:mid])
+		right := sort(nums[mid:])
+		return merge(left, right)
+	}
+	return sort(nums)
+}
