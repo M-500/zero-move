fafd779053f240ad2880418474f920cbd014bafe
wulinlin <18574945291@163.com>
2024-05-28 21:07:32 +0800

    支付服务相关

---
+++ go-zero-project/mall/service/pay/rpc/rpository/dao/model/pay_model.go
@@ -0,0 +1,19 @@
+package model
+
+import "gorm.io/gorm"
+
+// @Description
+// @Author 代码小学生王木木
+
+type PayModel struct {
+	gorm.Model
+	Uid    int64   `gorm:"comment:用户ID"`
+	Oid    int64   `gorm:"comment:订单ID"`
+	Amount float64 `gorm:"comment:产品金额"`
+	Source int     `gorm:"comment:支付方式，支付宝,微信"`
+	Status int     `gorm:"comment:支付状态"`
+}
+
+func (PayModel) TableName() string {
+	return "pay"
+}
