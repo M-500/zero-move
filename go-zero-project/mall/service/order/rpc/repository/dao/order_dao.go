858314f37ec5b82175f244dd10884e3b26115092
wulinlin <18574945291@163.com>
2024-05-28 20:35:43 +0800

    完善order的DAO层

--- go-zero-project/mall/service/order/rpc/repository/dao/order_dao.go
+++ go-zero-project/mall/service/order/rpc/repository/dao/order_dao.go
@@ -12,7 +12,7 @@ import (
 type OrderDAO interface {
 	Insert(ctx context.Context, data model.OrderModel) (int64, error) // 创建订单
 	Update(ctx context.Context, data model.OrderModel) error
-	Delete(ctx context.Context, id int64) (model.OrderModel, error)
+	Delete(ctx context.Context, id int64) error
 	FindById(ctx context.Context, id int64) (model.OrderModel, error)
 	FindListByUid(ctx context.Context, id int64) ([]model.OrderModel, error)
 }
@@ -22,26 +22,32 @@ type orderDAO struct {
 }
 
 func (o *orderDAO) Insert(ctx context.Context, data model.OrderModel) (int64, error) {
-	//TODO implement me
-	panic("implement me")
+	err := o.DB.WithContext(ctx).Model(&model.OrderModel{}).Create(&data).Error
+	return 0, err
 }
 
 func (o *orderDAO) Update(ctx context.Context, data model.OrderModel) error {
-	//TODO implement me
-	panic("implement me")
+	return o.DB.WithContext(ctx).Model(&model.OrderModel{}).Where("id = ?", data.ID).Updates(&data).Error
 }
 
-func (o *orderDAO) Delete(ctx context.Context, id int64) (model.OrderModel, error) {
-	//TODO implement me
-	panic("implement me")
+func (o *orderDAO) Delete(ctx context.Context, id int64) error {
+	return o.DB.WithContext(ctx).Delete(&model.OrderModel{}, id).Error
 }
 
 func (o *orderDAO) FindById(ctx context.Context, id int64) (model.OrderModel, error) {
-	//TODO implement me
-	panic("implement me")
+	var order model.OrderModel
+	err := o.DB.WithContext(ctx).Model(&model.OrderModel{}).Where("id = ?", id).Find(&order).Error
+	if err != nil {
+		return model.OrderModel{}, err
+	}
+	return order, nil
 }
 
 func (o *orderDAO) FindListByUid(ctx context.Context, id int64) ([]model.OrderModel, error) {
-	//TODO implement me
-	panic("implement me")
+	var orders []model.OrderModel
+	err := o.DB.WithContext(ctx).Model(&model.OrderModel{}).Where("uid = ?", id).Find(&orders).Error
+	if err != nil {
+		return nil, err
+	}
+	return orders, nil
 }
