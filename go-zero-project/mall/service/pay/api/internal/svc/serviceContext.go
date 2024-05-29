3e150c7116e9eee2fa2e0744a2b0305a7b20ed58
wulinlin <18574945291@163.com>
2024-05-29 12:02:26 +0800

    不高兴就喝水

--- go-zero-project/mall/service/pay/api/internal/svc/serviceContext.go
+++ go-zero-project/mall/service/pay/api/internal/svc/serviceContext.go
@@ -1,15 +1,20 @@
 package svc
 
 import (
+	"github.com/zeromicro/go-zero/zrpc"
 	"mall/service/pay/api/internal/config"
+	"mall/service/pay/rpc/payClient"
+	"mall/service/pay/rpc/types/pay"
 )
 
 type ServiceContext struct {
 	Config config.Config
+	PayRpc pay.PayClient
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
 		Config: c,
+		PayRpc: payClient.NewPay(zrpc.MustNewClient(c.PayRpc)),
 	}
 }
