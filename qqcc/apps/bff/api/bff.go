c44fa879940824b78eb77a7776b615d947dde28e
高桥琳介 <18574945291@163.com>
2024-06-02 21:27:26 +0800

    BFF层代码+1

--- qqcc/apps/bff/api/bff.go
+++ qqcc/apps/bff/api/bff.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/zeromicro/go-zero/rest/httpx"
+	"qqcc/pkg/xcode"
 
 	"qqcc/apps/bff/api/internal/config"
 	"qqcc/apps/bff/api/internal/handler"
@@ -26,6 +28,8 @@ func main() {
 	ctx := svc.NewServiceContext(c)
 	handler.RegisterHandlers(server, ctx)
 
+	// 注册自定义错误处理方法
+	httpx.SetErrorHandler(xcode.ErrHandler)
 	fmt.Printf("Starting server at %s:%d...\n", c.Host, c.Port)
 	server.Start()
 }
