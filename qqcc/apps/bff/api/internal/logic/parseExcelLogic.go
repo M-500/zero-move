8ba0c638b8ba35ea096c1ced17b3e011ef976348
wulinlin <18574945291@163.com>
2024-06-03 12:26:29 +0800

    暂存

--- qqcc/apps/bff/api/internal/logic/parseExcelLogic.go
+++ qqcc/apps/bff/api/internal/logic/parseExcelLogic.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"context"
+	"qqcc/apps/dump/rpc/types/dump"
 
 	"qqcc/apps/bff/api/internal/svc"
 	"qqcc/apps/bff/api/internal/types"
@@ -25,6 +26,12 @@ func NewParseExcelLogic(ctx context.Context, svcCtx *svc.ServiceContext) *ParseE
 
 func (l *ParseExcelLogic) ParseExcel(req *types.ParseExcelRequest) (resp *types.ParseExcelResponse, err error) {
 	// todo: add your logic here and delete this line
-
-	return
+	_, err = l.svcCtx.DumpRpc.ParserExcel(l.ctx, &dump.ParserRequest{
+		Filename: req.FileName,
+		Filepath: req.FilePath,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &types.ParseExcelResponse{}, nil
 }
